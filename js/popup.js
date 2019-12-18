37ce984a5ded36333279f577121035b95c6eebe8
nasimmhn <nasim.mahzoun@gmail.com>
2019-12-18 22:45:56 +0100

    Added images and thumbnails to "filmer page"

---
+++ js/popup.js
@@ -0,0 +1,154 @@
+
+tippy('#jumanji', {
+  content:
+    "Beskrivning: <br> Gänget är tillbaka i 'Jumanji: The Next Level', men spelet har ändrats. Nu kommer de bland annat behöva ta sig igenom både öken och snöberg för att klara spelet. <br><br> <img src='assets/trailer_thumbnail/trailer2.jpg' style='width:200px;'>",
+  delay: [50, 0],
+  distance: -250,
+  arrow: true,
+  hideOnClick: true,
+  maxWidth: 230,
+  animation: "scale"
+});
+
+tippy('#joker', {
+  content: "Beskrivning: <br> Arthur jobbar som en clown på dagtid, och försöker slå igenom som stand-up komiker på dagen, men det känns som skratten alltid är på hans bekostnad. <br><br> <img src='assets/trailer_thumbnail/joker.jpeg' style='width:200px;'>",
+  delay: [50, 0],
+  distance: -250,
+  arrow: true,
+  hideOnClick: true,
+  maxWidth: 230,
+  animation: "scale"
+});
+
+
+
+tippy('#jul', {
+  content: "Beskrivning: <br> Världsstjärnan Simon kommer hem till Sverige för att fira jul. I det lilla samhället han växte upp arrangerar hans bror Anders den årliga julkonserten och Simon har övertalats att vara med.<br><br> <img src='assets/trailer_thumbnail/jul.jpg' style='width:200px;'>",
+  delay: [50, 0],
+  distance: -250,
+  arrow: true,
+  hideOnClick: true,
+  maxWidth: 230,
+  animation: "scale"
+});
+
+
+tippy('#porträtt', {
+  content: "Beskrivning: <br> Bretagne, Frankrike, 1760. Konstnären Marianne har fått i uppdrag att göra ett porträtt av Héloïse, en ung kvinna som just lämnat klostret. Héloïse ska giftas bort och den presumtiva brudgummen vill se hur hon ser ut.<br><br> <img src='assets/trailer_thumbnail/portrait.jpg' style='width:200px;'>",
+  delay: [50, 0],
+  distance: -250,
+  arrow: true,
+  hideOnClick: true,
+  maxWidth: 230,
+  animation: "scale"
+});
+
+tippy('#starwars', {
+  content: "Beskrivning: <br> Ingen försvinner för alltid … Reys resa fortsätter i det sista kapitlet av den episka Skywalker-sagan som spänner över generationer.<br><br> <img src='assets/trailer_thumbnail/starwars.jpg' style='width:200px;'>",
+  delay: [50, 0],
+  distance: -250,
+  arrow: true,
+  hideOnClick: true,
+  maxWidth: 230,
+  animation: "scale"
+});
+
+tippy('#aladdin', {
+  content: "Beskrivning: <br> I denna spelfilmsversion av den animerade klassikern möter vi Anden, som har kraften att ge tre önskningar till den som har den magiska lampan. Aladdin är den gatsmarte småtjuven som förälskar sig i sultanens vackra dotter Jasmine. <br><br> <img src='assets/trailer_thumbnail/aladdin.jpg' style='width:200px;'>",
+  delay: [50, 0],
+  distance: -250,
+  arrow: true,
+  hideOnClick: true,
+  maxWidth: 230,
+  animation: "scale"
+});
+
+tippy('#dora', {
+  content: "Beskrivning: <br> Efter att ha tillbringat större delen av sitt liv med att upptäcka djungeln tillsammans med sina föräldrar var ändå inte Dora förberedd för det största äventyret i hennes liv - gymnasiet. <br><br> <img src='assets/trailer_thumbnail/dora.jpg' style='width:200px;'>",
+  delay: [50, 0],
+  distance: -250,
+  arrow: true,
+  hideOnClick: true,
+  maxWidth: 230,
+  animation: "scale"
+});
+
+tippy('#addams', {
+  content: "Beskrivning: <br> Ett animerat äventyr med den besynnerliga familjen Addams. Familjen består av pappa Gomez och hans fru Morticia, onkel Fester, farmor och barnen Pugsley och Wednesday, husets butler Lurch samt den vandrande handen Thing. <br><br> <img src='assets/trailer_thumbnail/addams.jpg' style='width:200px;'>",
+  delay: [50, 0],
+  distance: -250,
+  arrow: true,
+  hideOnClick: true,
+  maxWidth: 230,
+  animation: "scale"
+});
+
+tippy('#maleficent', {
+  content: "Beskrivning: <br> Vi följer den komplicerade relationen mellan Maleficent och den blivande drottningen Aurora. Tillsammans kämpar de mot nya motståndare för att skydda hedarna och de magiska varelser som bor där.<br><br> <img src='assets/trailer_thumbnail/maleficent.jpg' style='width:200px;'>",
+  delay: [50, 0],
+  distance: -250,
+  arrow: true,
+  hideOnClick: true,
+  maxWidth: 230,
+  animation: "scale"
+});
+
+tippy('#midway', {
+  content: "Beskrivning: <br> Den episka och sanna berättelsen om en underbemannad amerikansk flotta och de modiga marinsoldater och piloter som segrade över en mäktig motståndare. Med demokrati och frihet på spel besegrade de över den japanska flottan vid slaget om Midway.<br><br> <img src='assets/trailer_thumbnail/midway.jpg' style='width:200px;'>",
+  delay: [50, 0],
+  distance: -250,
+  arrow: true,
+  hideOnClick: true,
+  maxWidth: 230,
+  animation: "scale"
+});
+
+tippy('#totoro', {
+  content: "Beskrivning: <br>Systrarna Tsatsuki och Mei har flyttat till landet med sin pappa eftersom deras mamma ligger på ett sjukhus i närheten. När Mei utforskar det nya hemmet upptäcker hon att dedelar huset med några märkliga varelser och ännu konstigare grannar. Bland annat en stor lurvig varelse som hon kallar Totoro. <br><br> <img src='assets/trailer_thumbnail/totoro.jpg' style='width:200px;'>",
+  delay: [50, 0],
+  distance: -250,
+  arrow: true,
+  hideOnClick: true,
+  maxWidth: 230,
+  animation: "scale"
+});
+
+tippy('#mamma', {
+  content: "Beskrivning: <br>Föräldrar väntar hela livet på att deras barn ska säga ”Tack!” och barnen väntar i sin tur på ett ”Förlåt!”. Denna omöjliga ekvation är utgångspunkten i komedin Ring mamma!<br>Niki är 35 men lever sitt liv som en 25 åring. Plötsligt tvingas hon att ta tag i alla livsval och relationer på en gång. <br><br> <img src='assets/trailer_thumbnail/ring_mamma.jpg' style='width:200px;'>",
+  delay: [50, 0],
+  distance: -250,
+  arrow: true,
+  hideOnClick: true,
+  maxWidth: 230,
+  animation: "scale"
+});
+
+tippy('#panipat', {
+  content: "Beskrivning: <br>Filmen är baserad på det tredje slaget vid Panipat som ägde rum 1761 mellan Marathas och kungen av Afghanistan, Ahmad Shah Abdali. <br><br> <img src='assets/trailer_thumbnail/panipat.jpg' style='width:200px;'>",
+  delay: [50, 0],
+  distance: -250,
+  arrow: true,
+  hideOnClick: true,
+  maxWidth: 230,
+  animation: "scale"
+});
+
+tippy('#toystory', {
+  content: "Beskrivning: <br>Woody har alltid känt sig säker på var han hör hemma och att hans viktigaste uppgift är att ta hand om sitt barn, vare sig det är Andy eller Bonnie. Så när Bonnies nya hemmagjorda leksak Gaffe kallar sig själv för sopa och inte leksak, tar Woody på sig uppgiften att visa Gaffe varför det är så underbart att vara en leksak. <br><br> <img src='assets/trailer_thumbnail/toystory.jpg' style='width:200px;'>",
+  delay: [50, 0],
+  distance: -250,
+  arrow: true,
+  hideOnClick: true,
+  maxWidth: 230,
+  animation: "scale"
+});
+
+tippy('#corgi', {
+  content: "Beskrivning: <br>Rex är en nöjd corgi, han lever ett liv i lyx i sitt hem på Buckingham Palace. Trots stenhård konkurrens från de tre andra hundarna på slottet har han vunnit drottningens hjärta. Men hans självsäkra stil kan vara irriterande och under en middag med USA:s president råkar han orsaka en ytterst olämplig diplomatisk situation som får honom att skämmas. <br><br> <img src='assets/trailer_thumbnail/corgi.jpg' style='width:200px;'>",
+  delay: [50, 0],
+  distance: -250,
+  arrow: true,
+  hideOnClick: true,
+  maxWidth: 230,
+  animation: "scale"
+});
